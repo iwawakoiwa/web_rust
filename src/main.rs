4adc0c3150fba2c06bce04583cd93e1433987f71
iwa-koiwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-02-25 17:29:21 +0900

    不要なライブラリの使用を削除

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use axum::{
-    Router,
-    routing::get,
-};
+use axum::Router;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .nest_service("/",ServeDir::new("static"));
+        .fallback_service(ServeDir::new("static"));
     println!("server done");
 
     let listener = TcpListener::bind("0.0.0.0:80").await.unwrap();
-    axum::serve(listener,app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
