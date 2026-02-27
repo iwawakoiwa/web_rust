b788d364f4c4c4b39c0c9c945bcc3dfd9535c546
iwa-koiwa <220993652+iwa-koiwa@users.noreply.github.com>
2026-02-27 13:40:13 +0900

    下位のパスでもアクセスできるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use axum::Router;
+use axum::{Router, response::Html};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .fallback_service(ServeDir::new("static"));
+        .nest_service("/assets", ServeDir::new("static/assets"))
+        .fallback(|| async {
+            let html = tokio::fs::read_to_string("static/index.html")
+                .await
+                .unwrap_or_default();
+            Html(html)
+        });
     println!("server done");
 
     let listener = TcpListener::bind("0.0.0.0:80").await.unwrap();
